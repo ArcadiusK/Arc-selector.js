f8fbbb8cafc5d1f1bc2d4a2102aa7ae6e6fc66bf
Arcadius Kazimierski <arcadiusk@gmail.com>
2014-11-07 16:48:52 -0500

    completed all 16 tests of selector.js

--- src/selector.js
+++ src/selector.js
@@ -5,11 +5,63 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
     startEl = document.body;
   }
 
+        
+ 
+
+
+
+
+var walk_the_DOM = function walk(node, func) {
+    func(node);
+    node = node.firstChild;
+    while (node) {
+        walk(node, func);
+        node = node.nextSibling;
+    }
+};
+
+walk_the_DOM(startEl, function(node) {
+    if(node.nodeType == 1)
+    {
+   if (matchFunc(node)) resultSet.push(node);
+   console.log(node);
+     //   alert(node.nodeName + ' id: ' + node.id);
+    }
+ 
+});
+
+
+
+//var childNodes = startEl.childNodes;
+
+//         for(var i=0; i<childNodes.length; i++) {
+
+//            // console.log(childNodes[i]);
+// if (matchFunc(childNodes[i])) resultSet.push(childNodes[i])
+
+// //  console.log(matchFunc);
+//         };
+    
+
+
+
   // your code here
   // traverse the DOM tree and collect matching elements in resultSet
   // use matchFunc to identify matching elements
 
   return resultSet;
+};
+
+
+var $ = function(selector) {
+ var elements; 
+  var selectorMatchFunc = matchFunctionMaker(selector);
+   // console.log(selectorMatchFunc);
+  //  console.log(selector);
+  //console.log(selectorMatchFunc);
+  elements = traverseDomAndCollectElements(selectorMatchFunc);
+  //console.log(selectorMatchFunc);
+  return elements;
 };
 
 
@@ -40,7 +92,27 @@ var matchFunctionMaker = function(selector) {
     }
   } else if (selectorType === "class") {
     matchFunction = function(sampleDivEl) { 
-      return ((sampleDivEl.className.indexOf(selector.substring(1)) > -1)) 
+    // console.log(selector.substring(1));
+    //  console.log(selector+" ".substring(1)+"arc");
+
+
+
+
+
+
+// class="fdasfaf adfasfa adfad dasf photo asdfdasf"
+
+//class="photo dfafaf ad  af asdf"
+
+//clas="photo"
+
+//class="dfrfqwerqewer qwerewrqw photo"
+
+
+
+var with_space_front = " "+selector.substring(1);
+var with_space_rear = selector.substring(1)+" ";
+      return ((sampleDivEl.className.indexOf(with_space_front) > -1) ||  (sampleDivEl.className===selector.substring(1)) || (sampleDivEl.className.indexOf(with_space_rear) > -1)) 
     }
   } else if (selectorType === "tag.class") {
     
@@ -51,16 +123,22 @@ var matchFunctionMaker = function(selector) {
 
   } else if (selectorType === "tag") {
     matchFunction = function(sampleDivEl) {
-      return (sampleDivEl.tagName.toLowerCase() === selector);
+    console.log(typeof sampleDivEl);
+    if (sampleDivEl.tagName === undefined)
+    {
+      // console.log(sampleDivEl);
+      //  console.log(selector);
+      //   console.log(sampleDivEl === selector);
+      return (sampleDivEl === selector);
+      
+    } else {
+    return (sampleDivEl.tagName.toLowerCase() === selector);  
+    }
+    
     // define matchFunction for tag
+
     }
   }
   return matchFunction;
 };
 
-var $ = function(selector) {
-  var elements;
-  var selectorMatchFunc = matchFunctionMaker(selector);
-  elements = traverseDomAndCollectElements(selectorMatchFunc);
-  return elements;
-};
